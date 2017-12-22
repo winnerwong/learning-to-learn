9b8012903b637e0075a4ec1218eb52bea22efd69
Carmen Wong（司圭） <502539937@qq.com>
2017-12-23 00:48:59 +0800

    Add files via upload

---
+++ codecademy-learn+python+%281%29.py
@@ -0,0 +1,42 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+#给变量varible赋值了，print（变量名）时要不要加双引号，不加，如果加，就输出字符串（变量变成字符串）
+my_varible=10
+print(my_varible)
+print("my_varible")
+
+
+# In[2]:
+
+
+#定义函数def-return
+def spam():
+    eggs=5
+    return eggs
+print(spam())
+
+
+# In[5]:
+
+
+#字符长度与顺序
+ministry = "The Ministry of Silly Walks"
+print (len(ministry))
+print(ministry[2])
+
+
+# In[7]:
+
+
+print("a %s."%("apple"))
+
+
+# In[ ]:
+
+
+s
+
